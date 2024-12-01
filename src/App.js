b33c6211eb8ae7e770ed10f370164005f1191e65
amita250 <amitamitay95@gmail.com>
2024-12-01 15:03:29 +0200

    Updated App.js, App.css and App.test.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
@@ -6,17 +7,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Welcome to My New React App!
+        </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This website created by Amit
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
