09dbe5a0695a7a9c6c0a985ba2dceec229ff12e8
amita250 <amitamitay95@gmail.com>
2024-12-07 17:28:53 +0200

    Website change - add name form

--- src/App.js
+++ src/App.js
@@ -3,19 +3,34 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = React.useState('');
+  const [message, setMessage] = React.useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(`Hello, ${name}! Welcome to the site.`);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          Welcome to My New React App!
-        </h1>
-        <p>
-          This website created by Amit
-        </p>
+        <h1>Welcome to My New React App!</h1>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Enter your name:
+            <input 
+              type="text" 
+              value={name} 
+              onChange={(e) => setName(e.target.value)} 
+            />
+          </label>
+          <button type="submit">Submit</button>
+        </form>
+        {message && <p>{message}</p>}
       </header>
     </div>
   );
 }
+
 
 export default App;
